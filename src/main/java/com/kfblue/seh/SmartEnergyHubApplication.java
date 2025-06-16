918936466fef235841b548fa8ea40546150969d4
tmmk <tmmk520@gmail.com>
2025-06-16 17:41:42 +0800

    Initial commit: Smart Energy Hub project setup

---
+++ src/main/java/com/kfblue/seh/SmartEnergyHubApplication.java
@@ -0,0 +1,13 @@
+package com.kfblue.seh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEnergyHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEnergyHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kfblue/seh/SmartEnergyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfblue.seh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnergyHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
